47199e7f92498c1464f4566fe0d4471ce077c34b
Noam Raz <noamraz8@gmail.com>
2021-02-18 19:59:15 +0200

    Fix gradient

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::iter;
+
 use ndarray::{s, Array, Array1, ArrayView1, Ix1};
 use rand::{seq::SliceRandom, Rng};
 use rand_distr::{Distribution, UnitDisc};
@@ -23,9 +25,8 @@ impl DataPoint {
         for _ in 0..n {
             let tag = rng.gen_bool(0.5);
 
-            let data = UnitDisc.sample(&mut rng);
-            let data =
-                Array1::from([data[0] + 2. * (tag as i32 as f32) - 1., data[1], 1.].to_vec());
+            let data: [f32; 2] = UnitDisc.sample(&mut rng);
+            let data = Array1::from([data[0] + bool_to_float(tag), data[1], 1.].to_vec());
 
             let data_point = DataPoint { data, tag };
             data_points.push(data_point);
@@ -33,6 +34,10 @@ impl DataPoint {
 
         data_points
     }
+}
+
+fn bool_to_float(b: bool) -> f32 {
+    2. * (b as i32 as f32) - 1.
 }
 
 /// An implementation for the stochastic gradient descent function.
@@ -61,32 +66,36 @@ fn sgd<P>(
 }
 
 fn soft_svm_grad(data_point: &DataPoint, param_estimation: ArrayView1<'_, f32>) -> Array1<f32> {
-    let y = data_point.tag as i32 as f32;
+    let d = param_estimation.dim();
+
+    let y = bool_to_float(data_point.tag);
     let data = &data_point.data;
 
+    let weight_grad: Array1<_> = param_estimation
+        .slice(s![0..d - 1])
+        .iter()
+        .copied()
+        .chain(iter::once(0.))
+        .collect();
+
     if 1. - y * param_estimation.dot(data) >= 0. {
-        &param_estimation - &(C * y * data)
+        weight_grad - C * y * data
     } else {
-        param_estimation.to_owned()
+        weight_grad
     }
 }
 
 fn soft_svm_loss(data_points: &[DataPoint], param_estimation: &Array1<f32>) -> f32 {
     let w = param_estimation.slice(s![0..param_estimation.dim() - 1]);
 
-    let a = 0.5 * w.dot(&w);
-    let b = data_points
-        .iter()
-        .map(|point| {
-            let value = C * (1. - (point.tag as i32 as f32) * param_estimation.dot(&point.data));
-            value.max(0.)
-        })
-        .sum::<f32>();
-
-    dbg!(a);
-    dbg!(b);
-
-    0.
+    0.5 * w.dot(&w)
+        + data_points
+            .iter()
+            .map(|point| {
+                let value = C * (1. - bool_to_float(point.tag) * param_estimation.dot(&point.data));
+                value.max(0.)
+            })
+            .sum::<f32>()
 }
 
 fn render_data_point(point: &DataPoint) -> impl Node {
@@ -156,38 +165,21 @@ fn render_svg(data_points: &[DataPoint], model: ArrayView1<'_, f32>) -> Document
 
 fn main() {
     const LEARNING_RATE: f32 = 0.01;
-    const BATCH_SIZE: usize = 4;
+    const BATCH_SIZE: usize = 10;
 
-    let data_points = DataPoint::generate_test_data(40);
-
-    // for epochs in 0..100 {
-    //     let model = sgd(
-    //         &data_points,
-    //         Ix1(3),
-    //         epochs,
-    //         LEARNING_RATE,
-    //         BATCH_SIZE,
-    //         soft_svm_grad,
-    //     );
-
-    //     println!(
-    //         "model ran with {} epochs, with loss of {}",
-    //         epochs,
-    //         soft_svm_loss(&data_points, &model)
-    //     );
-
-    // }
+    let data_points = DataPoint::generate_test_data(100);
 
     let model = sgd(
         &data_points,
         Ix1(3),
-        1000,
+        10000,
         LEARNING_RATE,
         BATCH_SIZE,
         soft_svm_grad,
     );
 
     println!("model: {}", model);
+    println!("loss: {}", soft_svm_loss(&data_points, &model));
 
     svg::save("out.svg", &render_svg(&data_points, model.view())).unwrap();
 }

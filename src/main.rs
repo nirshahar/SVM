9349cc7fa9b2bc792966fc019d3d1c292bb17dd1
nir <nirshahar@mail.tau.ac.il>
2021-02-18 16:24:18 +0200

    pushpushon ki eyal bikesh
    Co-authored-by: Noam Raz <nmraz@users.noreply.github.com>
    Co-authored-by: eyalnesher <eyal.nesher@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use ndarray::{s, Array, Array1, ArrayView1, Ix1};
 use rand::{seq::SliceRandom, Rng};
-use rand_distr::{Distribution, UnitCircle, UnitDisc};
+use rand_distr::{Distribution, UnitDisc};
 use svg::{
-    node::element::{Circle, Ellipse, Line},
+    node::element::{Circle, Line},
     Document, Node,
 };
 
@@ -13,6 +13,27 @@ const VIEWBOX_INSET: f32 = 2. * RENDERED_POINT_RADIUS;
 struct DataPoint {
     data: Array1<f32>,
     tag: bool,
+}
+
+impl DataPoint {
+    fn generate_test_data(n: usize) -> Vec<Self> {
+        let mut data_points = Vec::new();
+        let mut rng = rand::thread_rng();
+
+        for _ in 0..n {
+            let tag = rng.gen_bool(0.5);
+
+            let data = UnitDisc.sample(&mut rng);
+            let data = Array1::from(
+                [data[0] - 2. * (1. + (tag as i32 as f32) * 2.), data[1], 1.].to_vec(),
+            );
+
+            let data_point = DataPoint { data, tag };
+            data_points.push(data_point);
+        }
+
+        data_points
+    }
 }
 
 /// An implementation for the stochastic gradient descent function.
@@ -69,27 +90,6 @@ fn soft_svm_loss(data_points: &[DataPoint], param_estimation: &Array1<f32>) -> f
     0.
 }
 
-impl DataPoint {
-    fn generate_test_data(n: usize) -> Vec<Self> {
-        let mut data_points = Vec::new();
-        let mut rng = rand::thread_rng();
-
-        for _ in 0..n {
-            let tag = rng.gen_bool(0.5);
-
-            let data = UnitDisc.sample(&mut rng);
-            let data = Array1::from(
-                [data[0] - 2. * (1. + (tag as i32 as f32) * 2.), data[1], 1.].to_vec(),
-            );
-
-            let data_point = DataPoint { data, tag };
-            data_points.push(data_point);
-        }
-
-        data_points
-    }
-}
-
 fn render_data_point(point: &DataPoint) -> impl Node {
     Circle::new()
         .set("cx", point.data[0])
@@ -98,47 +98,55 @@ fn render_data_point(point: &DataPoint) -> impl Node {
         .set("fill", if point.tag { "red" } else { "blue" })
 }
 
+fn get_point_on_model(model: ArrayView1<'_, f32>, (bound_x, bound_y): (f32, f32)) -> (f32, f32) {
+    if model[1] != 0 {
+        (bound_x, -(model[0] * bound_x + model[2]) / model[1])
+    } else {
+        (bound_x, bound_y)
+    }
+}
+
 fn render_line(
     model: ArrayView1<'_, f32>,
-    view_box_starting_pos: (f32, f32),
-    view_box_size: f32,
+    view_box_min: (f32, f32),
+    view_box_max: (f32, f32),
 ) -> impl Node {
-    todo!()
+    let (x1, y1) = get_point_on_model(model, view_box_min);
+    let (x2, y2) = get_point_on_model(model, view_box_max);
+
+    Line::new()
+        .set("x1", x1)
+        .set("y1", y1)
+        .set("x2", x2)
+        .set("y2", y2)
+        .set("stroke", "black")
 }
 
 fn render_svg(data_points: &[DataPoint], model: ArrayView1<'_, f32>) -> Document {
     let min_x = data_points
         .iter()
-        .fold(f32::INFINITY, |a, b| a.min(b.data[0]));
+        .fold(f32::INFINITY, |a, b| a.min(b.data[0]))
+        - VIEWBOX_INSET;
     let min_y = data_points
         .iter()
-        .fold(f32::INFINITY, |a, b| a.min(b.data[1]));
+        .fold(f32::INFINITY, |a, b| a.min(b.data[1]))
+        - VIEWBOX_INSET;
     let max_x = data_points
         .iter()
-        .fold(f32::NEG_INFINITY, |a, b| a.max(b.data[0]));
+        .fold(f32::NEG_INFINITY, |a, b| a.max(b.data[0]))
+        + VIEWBOX_INSET;
     let max_y = data_points
         .iter()
-        .fold(f32::NEG_INFINITY, |a, b| a.max(b.data[1]));
+        .fold(f32::NEG_INFINITY, |a, b| a.max(b.data[1]))
+        + VIEWBOX_INSET;
 
     let document = data_points
         .iter()
         .fold(
-            Document::new().set(
-                "viewBox",
-                (
-                    min_x - VIEWBOX_INSET,
-                    min_y - VIEWBOX_INSET,
-                    max_x - min_x + 2. * VIEWBOX_INSET,
-                    max_y - min_y + 2. * VIEWBOX_INSET,
-                ),
-            ),
+            Document::new().set("viewBox", (min_x, min_y, max_x - min_x, max_y - min_y)),
             |doc, point| doc.add(render_data_point(point)),
         )
-        .add(render_line(
-            model,
-            (min_x - VIEWBOX_INSET, min_y - VIEWBOX_INSET),
-            (max_x - min_x + 2. * VIEWBOX_INSET).max(max_y - min_y + 2. * VIEWBOX_INSET),
-        ));
+        .add(render_line(model, (min_x, min_y), (max_x, max_y)));
 
     document
 }
